05afee658e828a2434f0fb79f5b869a179090055
sumitkpal62 <sciencebuddy51@gmail.com>
2024-06-29 11:00:35 +0530

    setup routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <div className='bg-red-400'>Download App</div>
-  )
-}
+    <>
+      <div className="bg-red-400">Navbar</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
