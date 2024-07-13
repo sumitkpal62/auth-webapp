cf1ce1a976935954069faf59f8f2a148cd91076b
sumitkpal62 <sciencebuddy51@gmail.com>
2024-07-13 07:54:47 +0530

    script add for the deployment

--- api/index.js
+++ api/index.js
@@ -4,13 +4,19 @@ import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import { errorCheck } from "./middlewares/authMiddleware.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from "path";
 
 const PORT = 4000;
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
 
 // Connect DB
