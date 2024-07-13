84b6f14b8c39e26b4d5b18c8f5bd8a4e30275c4d
sumitkpal62 <sciencebuddy51@gmail.com>
2024-07-13 07:36:32 +0530

    About and Home pages completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import "./About.css";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="max-w-[80%] mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-4">About</h1>
+      <ul>
+        <ul className="listItem">
+          <span>Technologies Used:</span>
+          <li className="list">
+            <span>Frontend:</span> React, React-Router
+          </li>
+          <li className="list">
+            <span>Backend:</span> Node.js, Express
+          </li>
+          <li className="list">
+            <span>Database:</span> MongoDB
+          </li>
+          <li className="list">
+            <span>Authentication:</span> Google OAuth, Firebase
+          </li>
+          <li className="list">
+            <span>Security:</span> JSON Web Tokens (JWT), cookies
+          </li>
+        </ul>
+        <ul className="listItem">
+          <span>Key Features:</span>
+          <li className="list">
+            <span>CRUD Operations:</span> Full support for Create, Read, Update,
+            and Delete operations, allowing users to manage their data
+            effortlessly.
+          </li>
+          <li className="list">
+            <span>User Authentication:</span> Secure login via Google OAuth,
+            enhanced with Firebase for robust authentication mechanisms.
+          </li>
+          <li className="list">
+            <span>Seamless Navigation: </span>Smooth page transitions using
+            React-Router, ensuring a user-friendly experience.
+          </li>
+        </ul>
+        <ul className="listItem">
+          <span>Security and Efficiency:</span>
+          <li className="list">
+            <span>JWT and Cookies: </span>Ensures secure user sessions and state
+            management.
+          </li>
+          <li className="list">
+            <span>RESTful API: </span> Efficient communication between frontend
+            and backend, ensuring reliable data transactions.
+          </li>
+        </ul>
+      </ul>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="max-w-[80%] mx-auto">
+      <h1 className="text-2xl font-semibold text-center my-5">
+        Welcome to the HomePage
+      </h1>
+      <p className="mb-3">
+        Welcome to our cutting-edge web application, a seamless integration of
+        modern technologies designed to provide a robust and user-friendly
+        experience. This platform leverages the power of React for dynamic
+        front-end development, enhanced by React-Router for smooth navigation
+        across different pages. On the backend, we've harnessed the efficiency
+        of Node.js and Express to handle server-side operations, while MongoDB
+        serves as our flexible and scalable database solution. Together, these
+        technologies ensure a responsive and efficient user interface paired
+        with reliable data management.
+      </p>
+      <p className="mb-3">
+        Security and user authentication are paramount in our application. We've
+        implemented Google OAuth for streamlined and secure user login, allowing
+        users to authenticate via their Google accounts effortlessly.
+        Additionally, Firebase enhances our authentication processes, ensuring
+        robust security protocols. JSON Web Tokens (JWT) are utilized to
+        maintain secure sessions, and cookies are employed to manage user states
+        effectively. This combination guarantees that user data is protected
+        while offering a convenient and straightforward authentication process.
+      </p>
+      <p className="">
+        Our application also supports comprehensive CRUD (Create, Read, Update,
+        Delete) operations, enabling users to interact with their data
+        seamlessly. Whether it's creating new entries, reading existing data,
+        updating records, or deleting them, our platform ensures each operation
+        is executed smoothly. This functionality is underpinned by a RESTful API
+        that connects the frontend and backend, ensuring that all data
+        transactions are efficient and reliable. In essence, our web app is a
+        testament to modern web development practices, combining
+        state-of-the-art technology with user-centric design to deliver a
+        superior digital experience.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
